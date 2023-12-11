397b649c118b214c320a6b17ade8335c36e9e8c3
raulgc10 <raulgomezcantador@gmail.com>
2023-12-11 16:33:46 +0100

    utilizando perceval como modulo de github

--- script-test.py
+++ script-test.py
@@ -1,8 +1,6 @@
 from perceval.backends.core.github import GitHub
 
 def obtener_issues(repo_owner, repo_name):
-    # Credenciales de autenticación de GitHub (token de acceso)
-    token = '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'  # Reemplaza con tu token de acceso a GitHub
 
     # Crea una instancia del backend de GitHub
     github_repo = GitHub(owner=repo_owner, repository=repo_name)
@@ -17,6 +15,6 @@ def obtener_issues(repo_owner, repo_name):
 # Proporciona el nombre del propietario del repositorio y el nombre del repositorio
 propietario = 'raulgc10'
 nombre_repo = 'OSSanalyzer'
-
+tokenuser = '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'
 # Llama a la función para obtener issues
 obtener_issues(propietario, nombre_repo)
