86b359558f63a011732cbc220b4c20d07942cb72
raulgc10 <raulgomezcantador@gmail.com>
2024-01-21 12:59:21 +0100

    Se añaden logs y fichero de configuración

--- mainmenu.py
+++ mainmenu.py
@@ -1,13 +1,57 @@
-from flask import Flask
+from flask import Flask, render_template
+import obtener_issues
+import logging
+import json
+
+
+# Función para cargar la configuración del programa que viene de un fichero de configuración en formato JSON
+def configload(config_file):
+    with open(config_file, "r") as file:
+        configs = json.load(file)
+    return configs
+
+
+# Función para obtener el puerto
+def port_load():
+    app_port = config["mainconfig"]["port"]
+    return app_port
+
+
+# Función para obtener configuración de los logs
+def log_load():
+    log_level = config["log"]["level"]
+    log_filename = config["log"]["filename"]
+    log_format = config["log"]["format"]
+
+    return log_level, log_filename, log_format
+
+
+# Obtenemos el fichero de configuración
+config = configload("config/OSSAnalyzerconfig.json")
+
+# Cargamos el puerto y la config de logs
+port = port_load()
+logs = log_load()
 
 # Crea una instancia de la aplicación Flask
 app = Flask(__name__)
 
-# Define una ruta (endpoint) y la función que manejará las solicitudes en esa ruta
-@app.route('/repository_info', methods=['GET', 'POST', 'OPTIONS'])
-def mi_funcion():
-    return '¡Hola desde mi endpoint!'
+# Crea logs del programa
+logging.basicConfig(level=logs[0], filename=logs[1], format=logs[2])
 
-# Ejecuta la aplicación en el puerto 5000
+
+# Define la ruta principal en la que se darán diferentes opciones al usuario para obtener los datos de los repositorios de github
+@app.route('/', methods=['GET'])
+def index():
+    app.logger.info("Página principal solicitada")
+    return render_template("mainpage.html")
+
+
+# Define una ruta (endpoint) y la función que manejará las solicitudes en esa ruta
+# @app.route('/userrepo', methods=['GET', 'POST'])
+#
+# @app.route('/userrepo', methods=['GET', 'POST'])
+
+# Ejecuta la aplicación
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=port)
--- obtener_issues.py
+++ obtener_issues.py
@@ -1,16 +1,11 @@
 from perceval.backends.core.github import GitHub
 from urllib.parse import urlparse
 def obtener_issues_personal(repo_owner, repo_name):
-
     # Crea una instancia del backend de GitHub
     github_repo = GitHub(owner=repo_owner, repository=repo_name)
 
-    # Itera sobre los issues y muestra información básica
-    for issue in github_repo.fetch():
-        print(f"Título: {issue['data']['title']}")
-        print(f"Estado: {issue['data']['state']}")
-        print(f"URL: {issue['data']['reactions']}")
-        print("------")
+    return github_repo
+
 
 
 def obtener_issues_url(url_repo):
@@ -18,20 +13,11 @@ def obtener_issues_url(url_repo):
     parsed_url = urlparse(url_repo)
     path_parts = parsed_url.path.strip('/').split('/')
 
-    if len(path_parts) != 2:
-        print("La URL del repositorio no es válida.")
-        return
-
+    #Define el propietario y el nombre del repositorio
     owner, repo_name = path_parts
 
     # Crea una instancia del backend de GitHub
     github_repo = GitHub(owner=owner, repository=repo_name)
 
-    # Itera sobre los issues y muestra información básica
-    for issue in github_repo.fetch():
-        print(f"Título: {issue['data']['title']}")
-        print(f"Estado: {issue['data']['state']}")
-        print(f"URL: {issue['data']['html_url']}")
-        print("------")
-
+    return github_repo
 
