2c127b8ed3560bfb052d1632cb749d18a687a5a2
raulgc10 <raulgomezcantador@gmail.com>
2024-02-27 20:23:15 +0100

    cambio a visual studio code + js

---
+++ templates/mainpage.js
@@ -0,0 +1,7 @@
+const btn_userrepo = document.getElementById("btn_userrepo")
+const btn_url = document.getElementById("btn_url")
+const btn_others = document.getElementById("btn_others")
+
+btn_userrepo.onclick = () => {
+    window.location.href = "userrepo.html"
+}
